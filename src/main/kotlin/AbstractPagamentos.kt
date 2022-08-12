5c894e7054df9c3c2d3fc0caa576320a634a328a
Caio Ícaro <zeroaz819@gmail.com>
2022-08-11 23:05:21 -0300

    Criação da classe de compra de veiculos

--- src/main/kotlin/AbstractPagamentos.kt
+++ src/main/kotlin/AbstractPagamentos.kt
@@ -2,7 +2,7 @@ abstract class AbstractPagamentos {
     var entrada = 0.0
     var parcela = 0.0
 
-    abstract fun pagamentoAvista(lance: Double): Double
-    abstract fun pagamento12x(lance: Double): Double
-    abstract fun pagamento24x(lance: Double): Double
+    abstract fun pagamentoAvista(lance: Double)
+    abstract fun pagamento12x(lance: Double)
+    abstract fun pagamento24x(lance: Double)
 }
--- src/main/kotlin/CompraDeVeiculo.kt
+++ src/main/kotlin/CompraDeVeiculo.kt
@@ -1,2 +1,24 @@
-class CompraDeVeiculo {
+class CompraDeVeiculo : AbstractPagamentos() {
+    override fun pagamentoAvista(lance: Double) {
+        lance -= lance * 0.1
+        println("O valor do veiculo será: $lance")
+    }
+
+    override fun pagamento12x(lance: Double) {
+       entrada = lance * 0.3
+       parcela = (lance - entrada)/12
+       println("Para realizar a compra deve-se dar uma entrada de R$ $entrada " +
+               "e as parcelas ficarão em R$ $parcela sem juros!")
+    }
+
+    override fun pagamento24x(lance: Double) {
+        entrada =lance *0.2
+        parcela =(lance - entrada)/24
+        println("Para realizar a compra deve-se dar uma entrada de R$ $entrada " +
+                "e as parcelas ficarão em R$ $parcela sem juros!")
+    }
+
+    fun compraVeiculo() {
+
+    }
 }
