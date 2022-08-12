04f9b3a26f8e0311f997da0da3db727d35b108d5
Caio Ícaro <zeroaz819@gmail.com>
2022-08-12 00:46:03 -0300

    Criação do repositorioMarca

--- src/main/kotlin/RepositorioMarca.kt
+++ src/main/kotlin/RepositorioMarca.kt
@@ -6,7 +6,57 @@ class RepositorioMarca: Marca() {
         for (indice in marca.indices) {
             if (compareWithInput(name, indice)) {
                 temp = marca[indice]
+                println("Marca: ${nomeDaMarca(indice)}")
+                println("Pais de Origem: ${paisDeOrigemDaMarca(indice)}")
+                println("Pais de Produção: ${paisDeProducaoDaMarca(indice)}")
+                break
             }
         }
+        if (temp == null) {
+            println("Marca não encontrada!")
+        }
+    }
+
+    fun editarMarca(name: String) {
+        for (indice in marca.indices) {
+            try {
+                if (compareWithInput(name, indice)) {
+                    println("O que deseja alterar?" +
+                            "\n1 - Nome da Marca"   +
+                            "\n2 - País de Origem"  +
+                            "\n3 - País de produção")
+                    val respostaAlterar = readLine()?.toInt() ?: 0
+                    when(respostaAlterar) {
+                        1 -> {  println("Digite um novo nome para a marca: ")
+                                val respostaNome = readLine()?:"Marca genérica"
+                                marca[indice].setName(respostaNome)
+                        }
+                        2 -> {  println("Digite um novo pais de origem: ")
+                                val respostaPaisDeOrigem = readLine()?: "Pais genérico"
+                                marca[indice].setPaisDeOrigem(respostaPaisDeOrigem)
+                        }
+                        3 -> {  println("Digite um novo pais de produção: ")
+                                val respostaPaisDeProducao = readLine()?: "Pais genérico"
+                                marca[indice].setPaisDeProducao(respostaPaisDeProducao)
+                        }
+                        else -> println("Marca não encontrada!")
+                    }
+                }
+            } catch (e: Exception) {
+                println("Erro: Dados inválidos!")
+            }
+        }
+    }
+
+    fun removerMarca(name: String) {
+        for (indice in marca.indices) {
+            if (compareWithInput(name, indice)) {
+                marca.remove(getMarca(indice))
+            }
+        }
+    }
+
+    fun isEmpty():Boolean {
+        return marca.size == 0
     }
 }
