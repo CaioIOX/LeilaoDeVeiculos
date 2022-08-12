66f2cecf1f9b7e25f122a4a10630146ba8a8ca66
Caio Ícaro <zeroaz819@gmail.com>
2022-08-11 21:49:10 -0300

    Implementando método abstrato de pagamentos

--- src/main/kotlin/AbstractPagamentos.kt
+++ src/main/kotlin/AbstractPagamentos.kt
@@ -1,2 +1,8 @@
 abstract class AbstractPagamentos {
+    var entrada = 0.0
+    var parcela = 0.0
+
+    abstract fun pagamentoAvista(lance: Double): Double
+    abstract fun pagamento12x(lance: Double): Double
+    abstract fun pagamento24x(lance: Double): Double
 }
