72329bf091ebfcf2398834fbeedf80103061611a
Caio Ícaro <zeroaz819@gmail.com>
2022-08-11 23:58:50 -0300

    Cração da classe Marca

--- src/main/kotlin/Marca.kt
+++ src/main/kotlin/Marca.kt
@@ -1,9 +1,29 @@
-class Marca(nome: String, paisDeOrigem: String, paisDeProducao: String) {
+open class Marca() {
 
-    private val name = ""
-    private val paisDeOrigem = ""
-    private val paisDeProducao = ""
-    private var marca = arrayListOf<Marca>()
+    private var name = ""
+    private var paisDeOrigem = ""
+    private var paisDeProducao = ""
+    var marca = arrayListOf<Marca>()
+
+    fun Marca(name: String, paisDeOrigem: String, paisDeProducao: String) {
+        this.name = name
+        this.paisDeOrigem = paisDeOrigem
+        this.paisDeProducao = paisDeProducao
+    }
+
+    fun Marca() {}
+    fun getMarca(indice: Int): Marca {return marca[indice]}
+    fun compareWithInput(name: String, indice: Int): Boolean {return nomeDaMarca(indice) == name.lowercase()
+    }
+    fun getName(): String {return name}
+    fun setName(name: String) {this.name = name}
+    fun getPaisDeorigem():String {return paisDeOrigem}
+    fun setPaisDeOrigem(paisDeOrigem: String) {this.paisDeOrigem = paisDeOrigem}
+    fun getPaisDeProducao(): String {return paisDeProducao}
+    fun setPaisDeProducao(paisDeProducao: String) {this.paisDeProducao = paisDeProducao}
+    fun nomeDaMarca(indice: Int): String {return getMarca(indice).getName()}
+    fun paisDeOrigemDaMarca(indice: Int): String {return getMarca(indice).getPaisDeorigem()}
+    fun paisDeProducaoDaMarca(indice: Int): String {return getMarca(indice).getPaisDeProducao()}
 
 
 }
--- src/main/kotlin/RepositorioMarca.kt
+++ src/main/kotlin/RepositorioMarca.kt
@@ -1,2 +1,12 @@
-class RepositorioMarca {
+class RepositorioMarca: Marca() {
+
+    fun adicionar(m: Marca) {marca.add(m)}
+    fun procurarMarca(name: String) {
+        var temp: Marca? = null
+        for (indice in marca.indices) {
+            if (compareWithInput(name, indice)) {
+                temp = marca[indice]
+            }
+        }
+    }
 }
